c3967b2a5162c6fcf1e6db7d3bfb5ca63d323416
cassiemyy <cassiemyuan@gmail.com>
2025-12-08 22:52:57 -0500

    Update part 5 component
    adjusted to feedback, added component and correspondence, added createNewRep() helper

--- src/RollForSandwich1.java
+++ src/RollForSandwich1.java
@@ -1,24 +1,56 @@
-import java.util.Objects;
+import java.util.Random;
 
-/*
- * Representation invariant:
- *   - bread, protein, cheese, wildCard, and sauce are never null
- *   - Strings may be empty to represent no selection
+/**
+ * Concrete implementation of RollForSandwich using the secondary class.
  *
- * Abstraction function:
- *   a sandwich roll with the specified bread, protein, cheese, wildCard, and sauce
+ * @author Cassie Yuan
+ *
+ * @convention bread, protein, cheese, wildCard, sauce are not null. the empty
+ *             string "" represents no selection for each component.
+ *
+ * @correspondence this.bread = bread, this.protein = protein, this.cheese =
+ *                 cheese, this.wildCard = wildCard
  */
-
 public class RollForSandwich1 extends RollForSandwichSecondary {
 
-    //representation and constructor implementaitnon
+    /**
+     * Representation fields.
+     */
+
+    /**
+     * Bread choice.
+     */
     private String bread;
+
+    /**
+     * Protein choice.
+     */
     private String protein;
+
+    /**
+     * Cheese choice.
+     */
     private String cheese;
+
+    /**
+     * Wild card choice.
+     */
     private String wildCard;
+
+    /**
+     * Sauce choice.
+     */
     private String sauce;
 
-    public RollForSandwich1() {
+    /**
+     * Random number generator used by rollCategory.
+     */
+    private final Random rand;
+
+    /**
+     * Private helper to initialize the representation fields.
+     */
+    private void createNewRep() {
         this.bread = "";
         this.protein = "";
         this.cheese = "";
@@ -26,80 +58,194 @@ public class RollForSandwich1 extends RollForSandwichSecondary {
         this.sauce = "";
     }
 
-    //kernel methods
-    public void setBread(String b) {
-        this.bread = b;
+    /**
+     * Default constructor.
+     *
+     * @ensures this = new RollForSandwich1 with all components unselected
+     */
+    public RollForSandwich1() {
+        this.rand = new Random();
+        this.createNewRep();
     }
 
+    /**
+     * Kernel methods.
+     */
+
+    @Override
+    public String rollCategory(String[] options) {
+        assert options != null
+                && options.length > 0 : "Violation of: options is not null and not empty";
+        int index = this.rand.nextInt(options.length);
+        return options[index];
+    }
+
+    /**
+     * Clear the sandwich using the representation helper.
+     */
+    @Override
+    public void eatsWhole() {
+        this.createNewRep();
+    }
+
+    /**
+     * A sandwich is complete if all parts are non-empty strings.
+     *
+     * @return true iff all components are not empty
+     */
+    @Override
+    public boolean isComplete() {
+        return !this.bread.equals("") && !this.protein.equals("")
+                && !this.cheese.equals("") && !this.wildCard.equals("")
+                && !this.sauce.equals("");
+    }
+
+    /**
+     * Set bread choice.
+     *
+     * @param bread
+     */
+    @Override
+    public void setBread(String bread) {
+        assert bread != null : "Violation of: bread is not null";
+        this.bread = bread;
+    }
+
+    /**
+     * Get bread choice.
+     *
+     * @return bread
+     */
+    @Override
     public String getBread() {
         return this.bread;
     }
 
-    public void setProtein(String p) {
-        this.protein = p;
+    /**
+     * Set protein choice.
+     *
+     * @param protein
+     */
+    @Override
+    public void setProtein(String protein) {
+        assert protein != null : "Violation of: protein is not null";
+        this.protein = protein;
     }
 
+    /**
+     * Get protein choice.
+     *
+     * @return protein
+     */
+    @Override
     public String getProtein() {
         return this.protein;
     }
 
-    public void setCheese(String c) {
-        this.cheese = c;
+    /**
+     * Set cheese choice.
+     *
+     * @param cheese
+     */
+    @Override
+    public void setCheese(String cheese) {
+        assert cheese != null : "Violation of: cheese is not null";
+        this.cheese = cheese;
     }
 
+    /**
+     * Get cheese choice.
+     *
+     * @return cheese
+     */
+    @Override
     public String getCheese() {
         return this.cheese;
     }
 
-    public void setWildCard(String w) {
-        this.wildCard = w;
+    /**
+     * Set wild card choice.
+     *
+     * @param wildCard
+     */
+    @Override
+    public void setWildCard(String wildCard) {
+        assert wildCard != null : "Violation of: wildCard is not null";
+        this.wildCard = wildCard;
     }
 
+    /**
+     * Get wild card choice.
+     *
+     * @return wildCard
+     */
+    @Override
     public String getWildCard() {
         return this.wildCard;
     }
 
-    public void setSauce(String s) {
-        this.sauce = s;
+    /**
+     * Set sauce choice.
+     *
+     * @param sauce
+     */
+    @Override
+    public void setSauce(String sauce) {
+        assert sauce != null : "Violation of: sauce is not null";
+        this.sauce = sauce;
     }
 
+    /**
+     * Get sauce choice.
+     *
+     * @return sauce
+     */
+    @Override
     public String getSauce() {
         return this.sauce;
     }
 
     //standard methods
 
+    /**
+     * Clear the sandwich.
+     */
     @Override
     public void clear() {
-        this.bread = "";
-        this.protein = "";
-        this.cheese = "";
-        this.wildCard = "";
-        this.sauce = "";
+        this.createNewRep();
     }
 
+    /**
+     * Make a new instance of RollForSandwich1.
+     *
+     * @return new RollForSandwich1
+     */
     @Override
     public RollForSandwich newInstance() {
         return new RollForSandwich1();
     }
 
+    /**
+     * Transfer from source to this.
+     *
+     * @param source
+     */
     @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof RollForSandwich)) {
-            return false;
-        }
-        RollForSandwich other = (RollForSandwich) obj;
-        return Objects.equals(this.getBread(), other.getBread())
-                && Objects.equals(this.getProtein(), other.getProtein())
-                && Objects.equals(this.getCheese(), other.getCheese())
-                && Objects.equals(this.getWildCard(), other.getWildCard())
-                && Objects.equals(this.getSauce(), other.getSauce());
-    }
+    public void transferFrom(RollForSandwich source) {
+        assert source != null : "Violation of: source is not null";
+        assert source != this : "Violation of: source is not this";
+        assert source instanceof RollForSandwich1 : ""
+                + "Violation of: source is of dynamic type RollForSandwich1";
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getBread(), getProtein(), getCheese(),
-                getWildCard(), getSauce());
-    }
+        RollForSandwich1 localSource = (RollForSandwich1) source;
 
+        this.bread = localSource.bread;
+        this.protein = localSource.protein;
+        this.cheese = localSource.cheese;
+        this.wildCard = localSource.wildCard;
+        this.sauce = localSource.sauce;
+
+        localSource.createNewRep();
+
+    }
 }
